13b831d54c56f82856f9f23af5b16d539bd996ca
BertoPolo <bertopolo91@gmail.com>
2022-05-17 20:59:40 +0200

    starting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import "bootstrap-icons/font/bootstrap-icons.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
@@ -9,17 +11,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
+// on route =>  need path and element .... for 404  put path "*"
 
-export default App;
+export default App
