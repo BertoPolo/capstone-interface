34dad8005e935fcd539a32f4c4c733a954a82ac5
BertoPolo <bertopolo91@gmail.com>
2022-05-23 20:32:49 +0200

    adding states to Login, and enabling routers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
-// import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./components/Login"
+import Home from "./components/Home"
 
 function App() {
   return (
     <div className="">
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-          <Route path="/login" component={Login} />
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
-      </BrowserRouter> */}
-      <Login />
+      </BrowserRouter>
     </div>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,35 +1,46 @@
 import { Form, Button } from "react-bootstrap"
-// import { Navigate, useNavigate } from "react-router-dom"
+import { Navigate, useNavigate } from "react-router-dom"
+import { useState } from "react"
 
 //TODO
 //disable button if username is empty
 
 const Login = () => {
-  // const navigate = useNavigate()
-  // const handleSubmit = () => {
-  //   navigate("/home")
-  // }
+  const [usernameInput, setUsernameInput] = useState("")
+  const [passwordInput, setPasswordInput] = useState("")
+  const [isRemember, setIsRemember] = useState(false)
+
+  const navigate = useNavigate()
+  const handleSubmit = () => {
+    navigate("/home")
+  }
 
   return (
     <Form className="login-container">
       <div className="login-modal">
         <h4 className="mb-3">Login</h4>
         <Form.Group>
-          <Form.Control type="text" placeholder="Username" />
+          <Form.Control type="text" placeholder="Username" onChange={(e) => setUsernameInput(e.target.value)} />
         </Form.Group>
 
         <Form.Group>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" onChange={(e) => setPasswordInput(e.target.value)} />
         </Form.Group>
 
         <Form.Group>
-          <Form.Check type="checkbox" label="Remember me" className="login-small-font" />
+          <Form.Check type="checkbox" label="Remember me" className="login-small-font" onClick={() => setIsRemember(!isRemember)} />
         </Form.Group>
         <p className="login-small-font">Forgot the password?</p>
 
-        <Button variant="primary" type="submit">
-          Enter
-        </Button>
+        {usernameInput && passwordInput ? (
+          <Button variant="primary" type="submit" onClick={() => handleSubmit}>
+            Enter
+          </Button>
+        ) : (
+          <Button variant="secondary" type="submit" disabled>
+            Enter
+          </Button>
+        )}
       </div>
     </Form>
   )
