351b886ec0e351711669ffa6445defd4375dd6fd
BertoPolo <bertopolo91@gmail.com>
2022-06-17 12:52:45 +0200

    define slices

--- src/components/slices/routeSlice/routesSlice.js
+++ src/components/slices/routeSlice/routesSlice.js
@@ -6,7 +6,7 @@ const routesSlice = createSlice({
     initialState:{
         map:"",
         description:"",
-        pictures:"",
+        pictures:[],
         reviews:"",
         options:[], 
         garages:[], 
@@ -24,7 +24,37 @@ reducers:{
             map:action.payload,
         }
 },
+    changeDescription:(state,action)=>{
+        return{
+            ...state,
+            description:action.payload,
+        }
+},
+    changePictures:(state,action)=>{
+        return{
+            ...state,
+            pictures:action.payload,
+        }
+},
+    changeReviews:(state,action)=>{
+        return{
+            ...state,
+            reviews:action.payload,
+        }
+},
+    addOptions:(state,action)=>{
+        return{
+            ...state,
+            options:action.payload,
+        }
+},
+    addGarages:(state,action)=>{
+        return{
+            ...state,
+            garages:action.payload,
+        }
+},
 }})
 
 export default routesSlice.reducer
-export const {changeMap} = routesSlice.actions
+export const {changeMap,changeDescription,changePictures,changeReviews,addOptions,addGarages} = routesSlice.actions
--- src/components/slices/users/usersSclice.js
+++ src/components/slices/users/usersSclice.js
@@ -4,24 +4,80 @@ const citiesSlice = createSlice({
 
     name: 'citiesSlice',
     initialState:{
-        type: user/garageUser 
+        type: "",//user/garageUser 
         // token:"",
-        if garage user : reviews
-        if garage user  : description
-        if garage user : address
-        favRoutes:"", 
+        garageReviews:[],
+        garageDescription:"",
+        garageAddress:"",
+        favRoutes:[], 
         bike:"",
         avatar:"",
         friendList:[]
     },
-reducers:{
-    changeOriginCity:(state,action)=>{
+    reducers:{
+
+    defineTypeUser:(state,action)=>{
         return{
             ...state,
-            originCity:action.payload,
+            type:action.payload,
         }
 },
+    addGarageReviews:(state,action)=>{
+        return{
+            ...state,
+            garageReviews:action.payload,
+        }
+},
+    changeGarageDescription:(state,action)=>{
+        return{
+            ...state,
+            garageDescription:action.payload,
+        }
+},
+    changeGarageAdress:(state,action)=>{
+        return{
+            ...state,
+            garageAddress:action.payload,
+        }
+},
+    addFavRoutes:(state,action)=>{
+        return{
+            ...state,
+            garageAddress:action.payload,
+        }
+},
+//     deleteFavRoutes:(state,action)=>{
+//         return{
+//             ...state,
+//             garageAddress:action.payload,
+//         }
+// },
+    changeBike:(state,action)=>{
+        return{
+            ...state,
+            bike:action.payload,
+        }
+},
+    changeAvatar:(state,action)=>{
+        return{
+            ...state,
+            avatar:action.payload,
+        }
+},
+    addFriendList:(state,action)=>{
+        return{
+            ...state,
+            friendList:action.payload,
+        }
+},
+//     removeFromFriendList:(state,action)=>{
+//         return{
+//             ...state,
+//             friendList:action.payload,
+//         }
+// },
+
 }})
 
 export default citiesSlice.reducer
-export const {changeOriginCity} = citiesSlice.actions
+export const {addGarageReviews,defineTypeUser,changeGarageDescription,changeGarageAdresss,addFavRoutes,changeBike,changeAvatar,addFriendList} = citiesSlice.actions
