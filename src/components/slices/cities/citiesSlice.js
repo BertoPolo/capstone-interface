6ce2649ad8ed3b04fed0c2f8a46f82c330ed0469
BertoPolo <bertopolo91@gmail.com>
2022-06-17 13:04:16 +0200

    Update citiesSlice.js

--- src/components/slices/cities/citiesSlice.js
+++ src/components/slices/cities/citiesSlice.js
@@ -22,7 +22,61 @@ reducers:{
             originCity:action.payload,
         }
 },
+    changeOriginCountry:(state,action)=>{
+        return{
+            ...state,
+            originCountry:action.payload,
+        }
+},
+    changeOptStopCity:(state,action)=>{
+        return{
+            ...state,
+            optStopCity:action.payload,
+        }
+},
+    changeOptStopCountry:(state,action)=>{
+        return{
+            ...state,
+            optStopCountry:action.payload,
+        }
+},
+    changeDestinationCity:(state,action)=>{
+        return{
+            ...state,
+            destinationCity:action.payload,
+        }
+},
+    changeDestinationCountry:(state,action)=>{
+        return{
+            ...state,
+            destinationCountry:action.payload,
+        }
+},
+    changeWantStop:(state,action)=>{
+        return{
+            ...state,
+            wantStop:action.payload,
+        }
+},
+    changeOriginCityWeather:(state,action)=>{
+        return{
+            ...state,
+            originCityWeather:action.payload,
+        }
+},
+    changeOptCityWeather:(state,action)=>{
+        return{
+            ...state,
+            optCityWeather:action.payload,
+        }
+},
+    changeDestinationCityWeather:(state,action)=>{
+        return{
+            ...state,
+            destinationCityWeather:action.payload,
+        }
+},
 }})
 
 export default citiesSlice.reducer
-export const {changeOriginCity} = citiesSlice.actions
+export const {changeOriginCity,changeOriginCountry,changeOptStopCity,changeOptStopCountry,changeDestinationCity,changeDestinationCountry,changeWantStop,changeOriginCityWeather,changeOptCityWeather,changeDestinationCityWeather} = citiesSlice.actions
