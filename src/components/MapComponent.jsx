a3d3fa4195e0262d09b3a0cee13e2eb2a3f26e8e
BertoPolo <bertopolo91@gmail.com>
2022-06-25 11:49:28 +0200

    rendering a map

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -175,14 +175,53 @@
 // export default MapComponent
 // [END maps_react_map]
 
-const MapComponent = () => {
-  var mapboxgl = require("mapbox-gl/dist/mapbox-gl.js")
+// const MapComponent = () => {
+//   var mapboxgl = require("mapbox-gl/dist/mapbox-gl.js")
 
-  mapboxgl.accessToken = "pk.eyJ1IjoicG9sb2RlbGV2YXMiLCJhIjoiY2w0c2c3YWVtMGFsODNpcXE0dnQyeWFqaCJ9.l6LDinYwzrg5Gqmdnz3pBA" // generic try token
-  var map = new mapboxgl.Map({
-    container: "mycontainer",
-    style: "mapbox://styles/mapbox/streets-v11",
+//   mapboxgl.accessToken = "pk.eyJ1IjoicG9sb2RlbGV2YXMiLCJhIjoiY2w0c2c3YWVtMGFsODNpcXE0dnQyeWFqaCJ9.l6LDinYwzrg5Gqmdnz3pBA" // generic try token
+//   var map = new mapboxgl.Map({
+//     container: "mycontainer",
+//     style: "mapbox://styles/mapbox/streets-v11",
+//   })
+//   return <div id="mycontainer"></div>
+// }
+// export default MapComponent
+import React, { useRef, useEffect, useState } from "react"
+import mapboxgl from "!mapbox-gl" // eslint-disable-line import/no-webpack-loader-syntax
+mapboxgl.accessToken = "pk.eyJ1IjoicG9sb2RlbGV2YXMiLCJhIjoiY2w0c2c3YWVtMGFsODNpcXE0dnQyeWFqaCJ9.l6LDinYwzrg5Gqmdnz3pBA"
+
+const MapComponent = () => {
+  const mapContainer = useRef(null)
+  const map = useRef(null)
+  const [lng, setLng] = useState(-70.9)
+  const [lat, setLat] = useState(42.35)
+  const [zoom, setZoom] = useState(9)
+
+  useEffect(() => {
+    if (map.current) return // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    })
   })
-  return <div id="mycontainer"></div>
+  useEffect(() => {
+    if (!map.current) return // wait for map to initialize
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4))
+      setLat(map.current.getCenter().lat.toFixed(4))
+      setZoom(map.current.getZoom().toFixed(2))
+    })
+  })
+
+  return (
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  )
 }
 export default MapComponent
