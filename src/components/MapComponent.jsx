d220ff5169d1a5257ff829f2b250439e6ab73675
BertoPolo <bertopolo91@gmail.com>
2022-06-26 18:44:58 +0200

    Update MapComponent.jsx

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -188,40 +188,36 @@
 // export default MapComponent
 import React, { useRef, useEffect, useState } from "react"
 import mapboxgl from "!mapbox-gl" // eslint-disable-line import/no-webpack-loader-syntax
-// mapboxgl.accessToken = "pk.eyJ1IjoicG9sb2RlbGV2YXMiLCJhIjoiY2w0c2c3YWVtMGFsODNpcXE0dnQyeWFqaCJ9.l6LDinYwzrg5Gqmdnz3pBA"
+mapboxgl.accessToken = "pk.eyJ1IjoicG9sb2RlbGV2YXMiLCJhIjoiY2w0c2c3YWVtMGFsODNpcXE0dnQyeWFqaCJ9.l6LDinYwzrg5Gqmdnz3pBA"
 
 const MapComponent = () => {
   const mapContainer = useRef(null)
-  // const map = useRef(null)
+  const map = useRef(null)
   const [lng, setLng] = useState(-70.9)
   const [lat, setLat] = useState(42.35)
   const [zoom, setZoom] = useState(9)
+  const [MaxBounds, setMaxBounds] = useState([])
 
-  // useEffect(() => {
-  //   if (map.current) return // initialize map only once
-  //   map.current = new mapboxgl.Map({
-  //     container: mapContainer.current,
-  //     style: "mapbox://styles/mapbox/streets-v11",
-  //     center: [lng, lat],
-  //     zoom: zoom,
-  //   })
-  // })
-  // useEffect(() => {
-  //   if (!map.current) return // wait for map to initialize
-  //   map.current.on("move", () => {
-  //     setLng(map.current.getCenter().lng.toFixed(4))
-  //     setLat(map.current.getCenter().lat.toFixed(4))
-  //     setZoom(map.current.getZoom().toFixed(2))
-  //   })
-  // })
-
-  mapboxgl.accessToken = "pk.eyJ1IjoicG9sb2RlbGV2YXMiLCJhIjoiY2w0c2c3YWVtMGFsODNpcXE0dnQyeWFqaCJ9.l6LDinYwzrg5Gqmdnz3pBA"
-  const map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/streets-v11",
-    center: [-122.662323, 45.523751], // starting position
-    zoom: 12,
+  useEffect(() => {
+    if (map.current) return // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    })
   })
+  useEffect(() => {
+    if (!map.current) return // wait for map to initialize
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4))
+      setLat(map.current.getCenter().lat.toFixed(4))
+      setZoom(map.current.getZoom().toFixed(2))
+    })
+  })
+
+  /////////above is just the map. below is routes
+
   // set the bounds of the map
   const bounds = [
     [-123.069003, 45.395273],
