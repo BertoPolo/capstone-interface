d3a61cd5c47b81560573f1a648daf1f82937b2fb
BertoPolo <bertopolo91@gmail.com>
2022-07-08 17:46:46 +0200

    footer idea done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,35 @@
+import { Container, Row, Col } from "react-bootstrap"
+
+// center titles
+
 const Footer = () => {
   return (
     <>
-      <p>follow us in linkedIn ( me ) </p>
-      <p>basic info</p>
-      <p>contact phone</p>
+      <Container className="mt-3">
+        <Row>
+          <Col>
+            <h6>
+              <u>basic info</u>
+            </h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, labore! Accusantium quasi, vitae totam quaerat adipisci necessitatibus
+              veritatis debitis perferendis quisquam. Magnam vel quaerat illo ut, iste beatae ipsam quia!
+            </p>
+          </Col>
+          <Col>
+            <h6>
+              <u>Follow us</u>
+            </h6>
+            <p> Instagram Icon</p>
+            <p> LinkedIn Icon</p>
+          </Col>
+          <Col>
+            <h6>
+              <u>Contact us</u>
+            </h6>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
