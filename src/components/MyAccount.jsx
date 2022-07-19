0a10d9f5675ed28de119835281241e16e36a49b3
BertoPolo <bertopolo91@gmail.com>
2022-07-19 13:32:19 +0200

    Update MyAccount.jsx

--- src/components/MyAccount.jsx
+++ src/components/MyAccount.jsx
@@ -1,8 +1,12 @@
 import { Container, Form } from "react-bootstrap"
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 
 const MyAccount = () => {
   const navigate = useNavigate()
+  const [nameInput, setNameInput] = useState()
+  const [adressInput, setAdressInput] = useState()
+  const [passwordInput, setPasswordInput] = useState()
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -14,15 +18,15 @@ const MyAccount = () => {
         <h4 className="mb-3">Modify your data</h4>
 
         <Form.Group>
-          <Form.Control type="text" placeholder="Username" onChange={(e) => setNameInput(e.target.value)} />
+          <Form.Control type="text" placeholder="Username" value={nameInput} onChange={(e) => setNameInput(e.target.value)} />
         </Form.Group>
 
         <Form.Group>
-          <Form.Control type="text" placeholder="Shipping Adress" onChange={(e) => setAdressInput(e.target.value)} />
+          <Form.Control type="text" placeholder="Shipping Adress" value={adressInput} onChange={(e) => setAdressInput(e.target.value)} />
         </Form.Group>
 
         <Form.Group>
-          <Form.Control type="password" placeholder="Password" onChange={(e) => setPasswordInput(e.target.value)} />
+          <Form.Control type="password" placeholder="Password" value={passwordInput} onChange={(e) => setPasswordInput(e.target.value)} />
         </Form.Group>
       </Form>
     </>
