c4f10cd1dec3b000d1e25b4159cd868f5b4b8f46
BertoPolo <bertopolo91@gmail.com>
2022-07-30 17:34:40 +0200

    Update News.jsx

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -12,36 +12,6 @@ const News = () => {
     <>
       <MyNavbar />
 
-      <Container>
-        <div>
-          <Carousel className="">
-            <Carousel.Item>
-              <img className="d-block m-auto carouselImg" src="../../../IMG_8830.jpg" alt="First slide" />
-              <Carousel.Caption>
-                <h3>First - autor's name</h3>
-                <p>Country and route's name</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img className="d-block m-auto carouselImg" src="../../../IMG_8830.jpg" alt="Second slide" />
-
-              <Carousel.Caption>
-                <h3>Second - autor's name</h3>
-                <p>Country and route's name</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img className="d-block m-auto carouselImg" src="../../../IMG_8830.jpg" alt="Third slide" />
-
-              <Carousel.Caption>
-                <h3>Third - autor's name</h3>
-                <p>Country and route's name</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
-        </div>
-      </Container>
-
       <Container className="mt-5">
         <Row>
           <Col xs={2}>
