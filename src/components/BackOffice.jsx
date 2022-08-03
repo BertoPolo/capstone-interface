4ace257b90694a069d6657ccdb1e5a044510bf61
BertoPolo <bertopolo91@gmail.com>
2022-08-03 20:52:10 +0200

    preparing Backoffice

--- src/components/BackOffice.jsx
+++ src/components/BackOffice.jsx
@@ -1,21 +1,32 @@
 import { Form, Button } from "react-bootstrap"
 import MyNavbar from "./MyNavbar"
+import { useNavigate } from "react-router-dom"
 
-// search and delete user
+const BackOffice = () => {
+    const navigate = useNavigate()
 
-<>
-    <MyNavbar />
-    <Form className="login-container" onSubmit={(e) => handleSubmit(e)}>
-        <h4 className="mb-3">Modify your data</h4>
+    // search and delete user
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        navigate("/home")
+    }
+
+    <>
+        <MyNavbar />
+        <Form className="login-container" onSubmit={(e) => handleSubmit(e)}>
+            <h4 className="mb-3">Modify your data</h4>
 
 
 
-        <Form.Group>
-            <Form.Control type="text" placeholder="User name" value={nameInput} onChange={(e) => setNameInput(e.target.value)} />
-        </Form.Group>
+            <Form.Group>
+                <Form.Control type="text" placeholder="User name" value={nameInput} onChange={(e) => setNameInput(e.target.value)} />
+            </Form.Group>
 
 
-        <Button type="submit" disabled={ } > Submit </Button>
-        <Button variant="danger" onClick={() => navigate("/home")}>Back</Button>
-    </Form>
-</>
+            <Button type="submit" disabled > Submit </Button>
+            <Button variant="danger" onClick={() => navigate("/home")}>Back</Button>
+        </Form>
+    </>
+}
+
+export default BackOffice
