9a5b8ff44c30e457e7050e4936138bd65961f730
BertoPolo <bertopolo91@gmail.com>
2022-08-08 16:24:07 +0200

    changing component. iframes its bugged

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,57 +1,29 @@
-import { Container, Carousel, Col, Row, Dropdown } from "react-bootstrap"
-import MyNavbar from "./MyNavbar"
-import Footer from "./Footer"
-import HomeItem from "./HomeItem"
-import CategoriesMenu from "./CategoriesMenu"
-// import { useNavigate } from "react-router-dom"
+import { Form } from "react-bootstrap"
+import { useNavigate } from "react-router-dom"
+
 
 const ContactUs = () => {
   return (
     <>
-      <MyNavbar />
+      <h1>Contact Us</h1>
+      <ol>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+        <br />
+        <li> </li>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+        <li> </li>
 
-      <Container className="mt-5">
-        <Row>
-          <Col xs={2}>
-            <h4>CATEGORIES</h4>
-            <CategoriesMenu />
+      </ol>
 
-            <hr />
-            <h4>BRANDS</h4>
+      {/* google map where we are located */}
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d910.1302628522483!2d4.265224524993351!3d39.889890400498075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2ses!4v1659966174291!5m2!1sen!2ses" style={{ border: "0" }, { width: "600" }, { height: "450" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
-            <Dropdown>
-              <Dropdown.Toggle variant="warning">Dropdown Button</Dropdown.Toggle>
-
-              <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Brand 1</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Brand 2</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Brand 3</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Col>
-
-          <Col xs={10}>
-            <Col>
-              <Row>
-                {/* map items with math random */}
-                <Col>
-                  <HomeItem />
-                </Col>
-                <Col>
-                  <HomeItem />
-                </Col>
-                <Col>
-                  <HomeItem />
-                </Col>
-                <Col>
-                  <HomeItem />
-                </Col>
-              </Row>
-            </Col>
-            <Footer />
-          </Col>
-        </Row>
-      </Container>
     </>
   )
 }
