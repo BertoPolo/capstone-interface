e0c90e5865ce4f4919202e1436c005867b8e5703
BertoPolo <bertopolo91@gmail.com>
2022-08-09 12:15:15 +0200

    iframe bug solved and , change OL to UL

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -6,7 +6,7 @@ const ContactUs = () => {
   return (
     <>
       <h1>Contact Us</h1>
-      <ol>
+      <ul>
         <li> </li>
         <li> </li>
         <li> </li>
@@ -19,11 +19,10 @@ const ContactUs = () => {
         <li> </li>
         <li> </li>
 
-      </ol>
+      </ul>
 
       {/* google map where we are located */}
-      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d910.1302628522483!2d4.265224524993351!3d39.889890400498075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2ses!4v1659966174291!5m2!1sen!2ses" style={{ border: "0" }, { width: "600" }, { height: "450" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d910.1302628522483!2d4.265224524993351!3d39.889890400498075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2ses!4v1659966174291!5m2!1sen!2ses" style={{ border: "0", width: "50vw", height: "50vh" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </>
   )
 }
