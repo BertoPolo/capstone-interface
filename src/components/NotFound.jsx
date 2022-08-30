7a13d31ad1486057e0806b86ed4fcbf807417d2f
BertoPolo <bertopolo91@gmail.com>
2022-08-30 17:36:54 +0200

    adding return button

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,4 +1,4 @@
-import { Container } from "react-bootstrap"
+import { Container, Button } from "react-bootstrap"
 import MyNavbar from "./MyNavbar"
 
 const NotFound = () => {
@@ -8,6 +8,7 @@ const NotFound = () => {
       <Container className="">
         <h1 className="d-flex justify-content-center align-content-center">404 NOT FOUND</h1>
         <img src="" alt="" />
+        <Button variant="primary"> Return home</Button>
       </Container>
     </>
   )
