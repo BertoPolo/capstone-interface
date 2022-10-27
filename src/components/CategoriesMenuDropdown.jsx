11d52e260e83a751c5d57c9a1d5615e523d2a54e
BertoPolo <bertopolo91@gmail.com>
2022-10-27 19:59:09 +0200

    Update CategoriesMenuDropdown.jsx

--- src/components/CategoriesMenuDropdown.jsx
+++ src/components/CategoriesMenuDropdown.jsx
@@ -1,7 +1,26 @@
-import { Accordion, Card, Dropdown } from "react-bootstrap"
+import { Dropdown } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
 
 const CategoriesMenuDropdown = () => {
-    return
+
+    // fetch all "categories"  and fill dropdown with them
+
+
+    const getMainCategories = () => { }
+    const getCategories = () => { }
+
+    return (
+        <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Categories
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    )
 }
 export default CategoriesMenuDropdown
