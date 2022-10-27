59d7597f73f8b8a0382784345c69b324db2d6767
BertoPolo <bertopolo91@gmail.com>
2022-10-27 20:07:52 +0200

    Update CategoriesMenuDropdown.jsx

--- src/components/CategoriesMenuDropdown.jsx
+++ src/components/CategoriesMenuDropdown.jsx
@@ -1,13 +1,33 @@
 import { Dropdown } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
+import { addItems } from "../slices/items/itemsSlice"
 
 const CategoriesMenuDropdown = () => {
 
-    // fetch all "categories"  and fill dropdown with them
+    const items = useSelector((state) => state.itemsSlice.items);
 
 
-    const getMainCategories = () => { }
-    const getCategories = () => { }
+    const getMainCategories = async () => {
+
+        try {
+            const response = await fetch(`${process.env.React_APP_SERVER}` || `${process.env.React_APP_LOCAL_SERVER}mainCategories/all`);
+            const data = await response.json();
+            // dispatch(addBrands(data));
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    const getCategories = async () => {
+        try {
+            const response = await fetch(`${process.env.React_APP_SERVER}` || `${process.env.React_APP_LOCAL_SERVER}categories/all`);
+            const data = await response.json();
+            // dispatch(addBrands(data));
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <Dropdown>
@@ -16,9 +36,10 @@ const CategoriesMenuDropdown = () => {
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
+                {/* categories.map((category)=>{}) */}
+                {/* mainCategories.map((mCat)=>{}) */}
                 <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
                 <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
     )
