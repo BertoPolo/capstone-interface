62c3e9a3d603d7f0986d7c1776e61822be74ce6f
BertoPolo <bertopolo91@gmail.com>
2022-10-29 10:28:43 +0200

    adding navbar logo

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -41,7 +41,7 @@ const MyNavbar = () => {
       <Navbar expand="lg" className="sticky-top navbarBg">
         <Navbar.Brand href="/home">
           Stuff To Route
-          {/* <Image src={`${process.env.PUBLIC_URL}/mbIcon.png`} alt="main logo" /> */}
+          <Image src={`${process.env.PUBLIC_URL}/mbIcon.png`} alt="main logo" style={{ width: "1.5rem", marginLeft: "1rem" }} />
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse>
