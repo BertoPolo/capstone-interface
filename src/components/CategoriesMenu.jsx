ab60aa1a7e480b135d87fc851eba11fdf754afb7
BertoPolo <bertopolo91@gmail.com>
2022-10-31 16:15:08 +0100

    Update CategoriesMenu.jsx

--- src/components/CategoriesMenu.jsx
+++ src/components/CategoriesMenu.jsx
@@ -1,8 +1,10 @@
 import { Accordion, Card } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
+import { useEffect } from "react"
 import { toggleIsOnCategory, toggleIsOnBrands } from "../slices/sheets/sheetsSlice"
 import { addItems } from "../slices/items/itemsSlice"
 import { addCategories } from "../slices/categories/categoriesSlice"
+// import { addMainCategories } from "../slices/mainCategories/mainCategoriesSlice"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -11,9 +13,22 @@ function CategoriesMenu() {
 
   const items = useSelector((state) => state.itemsSlice.items);
   const brands = useSelector((state) => state.brandsSlice.brands);
-  const { categories, mainCategories } = useSelector((state) => state.categoriesSlice);
+  const categories = useSelector((state) => state.categoriesSlice.categories);
+  // const mainCategories = useSelector((state) => state.categoriesSlice.mainCategories);
 
   const dispatch = useDispatch()
+
+  const getCategories = async () => {
+    try {
+      const response = await fetch(`${process.env.React_APP_SERVER}` || `${process.env.React_APP_LOCAL_SERVER}categories/all`);
+      const data = await response.json();
+      dispatch(addCategories(data));
+      console.log(data)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 
   const getByMainCategory = async (mainCat) => {
@@ -58,17 +73,41 @@ function CategoriesMenu() {
     theme: "dark",
   });
 
+  useEffect(() => {
+    getCategories()
+  }, [])
+
   return (
     <>
-      {/* Main categories are hard coded for demo purposes,they should be maped alphabetically*/}
+
       <h4>CATEGORIES</h4>
       <Accordion>
         {categories.map((element) => {
+          return (
+            <Card key={element._id}>
+              <Card.Header>
+                <Accordion.Toggle as={Card.Header} eventKey={element._id} className="pointer" >
+                  {element.mainCategory.mainCategories}
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey={element._id}>
+                <Card.Body>
+                  <p className="pointer" onClick={() => getByCategory(element._id)}>{element.categories}</p>
 
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+
+          )
+
+
+          // mapear los main categories
+          // dentro, hacer otro map con las categories y checkeando que tiene la misma maincat
 
 
         })}
-        <Card>
+
+        {/* <Card>
           <Card.Header>
             <Accordion.Toggle as={Card.Header} eventKey="0" className="pointer" >
               Lights
@@ -171,7 +210,7 @@ function CategoriesMenu() {
 
             </Card.Body>
           </Accordion.Collapse>
-        </Card>
+        </Card> */}
       </Accordion>
     </>
 
