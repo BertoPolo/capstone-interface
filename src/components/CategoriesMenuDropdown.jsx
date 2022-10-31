d354c994787566d07edad096ef45af7d9421a3ab
BertoPolo <bertopolo91@gmail.com>
2022-10-31 18:24:52 +0100

    Update CategoriesMenuDropdown.jsx

--- src/components/CategoriesMenuDropdown.jsx
+++ src/components/CategoriesMenuDropdown.jsx
@@ -2,32 +2,34 @@ import { Dropdown, Accordion, Card, Button } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
 import { addItems } from "../slices/items/itemsSlice"
 import { addCategories } from "../slices/categories/categoriesSlice"
+import { addMainCategories } from "../slices/mainCategories/mainCategoriesSlice"
+
 
 const CategoriesMenuDropdown = () => {
 
     const items = useSelector((state) => state.itemsSlice.items);
-    const { categories, mainCategories } = useSelector((state) => state.categoriesSlice);
-    // const mainCategories = useSelector((state) => state.mainCategoriesSlice.mainCategories);
+    const categories = useSelector((state) => state.categoriesSlice.categories);
+    const mainCategories = useSelector((state) => state.mainCategoriesSlice.mainCategories);
 
     const dispatch = useDispatch()
 
-    // const getMainCategories = async () => {
+    const getMainCategories = async () => {
 
-    //     try {
-    //         const response = await fetch(`${process.env.React_APP_SERVER}` || `${process.env.React_APP_LOCAL_SERVER}mainCategories/all`);
-    //         const data = await response.json();
-    //         dispatch(addCategories(data));
+        try {
+            const response = await fetch(`${process.env.React_APP_SERVER}` || `${process.env.React_APP_LOCAL_SERVER}mainCategories/all`);
+            const data = await response.json();
+            if (data) dispatch(addMainCategories(data));
 
-    //     } catch (error) {
-    //         console.log(error)
-    //     }
-    // }
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     const getCategories = async () => {
         try {
             const response = await fetch(`${process.env.React_APP_SERVER}` || `${process.env.React_APP_LOCAL_SERVER}categories/all`);
             const data = await response.json();
-            // dispatch(addBrands(data));
+            if (data) dispatch(addCategories(data));
 
         } catch (error) {
             console.log(error)
@@ -42,14 +44,12 @@ const CategoriesMenuDropdown = () => {
                 <Card>
                     <Card.Header>
                         <Accordion.Toggle as={Card.Header} eventKey="0" className="pointer" >
-                            Lights
+                            Main category's name
                         </Accordion.Toggle>
                     </Card.Header>
                     <Accordion.Collapse eventKey="0">
                         <Card.Body>
-                            {/* <p className="pointer" onClick={() => getByCategory(element._id)}>Front Lights</p>
-                            <p className="pointer" onClick={() => getByCategory(element._id)}>Rear Lights</p>
-                            <p className="pointer" onClick={() => getByCategory(element._id)}>Signal Lights</p> */}
+                            {/* <p className="pointer" onClick={() => getByCategory(element._id)}>Front Lights</p>*/}
                         </Card.Body>
                     </Accordion.Collapse>
                 </Card>
