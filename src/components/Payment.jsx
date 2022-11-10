0cacb642ab7ea04333155983583765251c5faa64
BertoPolo <bertopolo91@gmail.com>
2022-11-10 17:14:22 +0100

    Update Payment.jsx

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,10 +1,38 @@
-import { Modal, Button, Dropdown } from "react-bootstrap"
+import { Modal, Button, Dropdown, Container, Form } from "react-bootstrap"
 import { useSelector, useDispatch } from "react-redux"
 import { useNavigate } from "react-router-dom";
+import Stripe from 'stripe';
 
 
 const Payment = () => {
-    return
+
+    // show user adress
+    // button to go to change adress
+
+    //form to pay
+
+    const stripe = new Stripe('sk_test_...');
+
+    (async () => {
+        const customer = await stripe.customers.create({
+            email: 'customer@example.com',
+        });
+
+        console.log(customer.id);
+    })();
+
+    return (
+        <Container>
+            <Form>
+                <Form.Control>
+
+                </Form.Control>
+            </Form>
+
+        </Container>
+
+    )
+
 }
 
 export default Payment
