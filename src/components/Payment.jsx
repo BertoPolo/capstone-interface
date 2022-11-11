824b04f6fd47c3816aad2f1a7e85a6fc550bee6c
BertoPolo <bertopolo91@gmail.com>
2022-11-11 16:57:39 +0100

    .

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,7 +1,6 @@
 import { Modal, Button, Dropdown, Container, Form } from "react-bootstrap"
 import { useSelector, useDispatch } from "react-redux"
 import { useNavigate } from "react-router-dom";
-import Stripe from 'stripe';
 
 
 const Payment = () => {
