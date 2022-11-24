b36831caf409aebd5fce4678f62bce779749f369
BertoPolo <bertopolo91@gmail.com>
2022-11-24 07:29:39 +0100

    Update sheetsSlice.js

--- src/slices/Sheets/sheetsSlice.js
+++ src/slices/Sheets/sheetsSlice.js
@@ -136,6 +136,7 @@ export const {
   toggleIsOnSingleItem,
   toggleIsOnCategory,
   toggleIsOnUserSearch,
+  toggleIsOnBrands,
   toggleIsOnItemSearch,
   toggleIsOnCreateNewItem,
 } = sheetsSlice.actions
