a36ec91247b573d64e6b50fea42b8abaf804e876
BertoPolo <bertopolo91@gmail.com>
2022-12-12 19:41:25 +0100

    responsiveness NavFliter

--- src/components/NavFilter.jsx
+++ src/components/NavFilter.jsx
@@ -139,7 +139,7 @@ const NavFilter = () => {
                     <Form className="d-flex justify-content-between w-100 align-items-center" onSubmit={(e) => getFilteredItems(e)}>
                         {/*PRICE SORTING */}
                         <Dropdown >
-                            <Dropdown.Toggle variant="outline">{selectSorting || "Choose sorting"}</Dropdown.Toggle>
+                            <Dropdown.Toggle variant="outline">{selectSorting || "Sorting by"}</Dropdown.Toggle>
                             <Dropdown.Menu title="Price sorting" id="basic-nav-dropdown">
                                 <Dropdown.Item onSelect={() => { setSorting("price"); setSelectSorting("Ascendant") }}>Price ascendant</Dropdown.Item>
                                 <Dropdown.Item onSelect={() => { setSorting("-price"); setSelectSorting("Descendant") }}>Price descendant</Dropdown.Item>
@@ -160,14 +160,6 @@ const NavFilter = () => {
                         </Dropdown>
 
                         {/* BY PRICE RANGE */}
-                        {/* <Form.Group >
-                            <span>From </span>
-                            <Form.Control type="number" value={minPrice} min="0" max="1000" onChange={(e) => setMinPrice(e.target.value)} />
-                        </Form.Group>
-                        <Form.Group >
-                            <span>To </span>
-                            <Form.Control type="number" value={maxPrice} min="0" max="1000" onChange={(e) => setMaxPrice(e.target.value)} />
-                        </Form.Group> */}
 
                         <MultiRangeSlider
                             min={0}
@@ -179,17 +171,32 @@ const NavFilter = () => {
                             onInput={(e) => {
                                 handleInput(e);
                             }}
+                            className="d-none d-md-block"
                         />
+
+                        <Form.Control type="number" value={minPrice} min="0" max="1000" className="d-md-none mr-1" onChange={(e) => setMinPrice(e.target.value)} />
+                        <Form.Control type="number" value={maxPrice} min="0" max="1000" className="d-md-none mr-1" onChange={(e) => setMaxPrice(e.target.value)} />
+
                         <Button type="submit" className="d-flex">Enter Filters</Button>
 
                     </Form>
 
                 </Row>
             </Container>
-
+            {/* <Form.Group >
+                            <span>From </span>
+                            <Form.Control type="number" value={minPrice} min="0" max="1000" onChange={(e) => setMinPrice(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group >
+                            <span>To </span>
+                            <Form.Control type="number" value={maxPrice} min="0" max="1000" onChange={(e) => setMaxPrice(e.target.value)} />
+                        </Form.Group> */}
 
         </>
     )
 }
+
+
+
 
 export default NavFilter
