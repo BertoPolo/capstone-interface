1a8d5b9585d6ad067e545d27cc9396d70f984510
BertoPolo <bertopolo91@gmail.com>
2023-11-29 21:52:41 +0100

    reducing stripe loading

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,26 +1,46 @@
 import { Container, Row } from "react-bootstrap"
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 const LazyCheckoutForm = React.lazy(() => import('./Payment_CheckOutForm'));
 
 
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
-const Payment = () => (
-    <Elements stripe={stripePromise}>
-        <Container className="mt-4">
-            <Row>
-                <div className="d-flex justify-content-center w-100">
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <LazyCheckoutForm />
-                    </Suspense>
+const Payment = () => {
+    const [stripeLoaded, setStripeLoaded] = useState(false);
+    const location = useLocation();
 
-                </div>
-            </Row>
-        </Container>
-    </Elements>
-);
+    useEffect(() => {
+        if (location.pathname === "/payment") {
+            setStripeLoaded(true);
+        }
+    }, [location]);
+
+    let stripePromise;
+    if (stripeLoaded) {
+        stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+    }
+
+    return (
+
+        <>
+            {stripeLoaded &&
+                <Elements stripe={stripePromise}>
+                    <Container className="mt-4">
+                        <Row>
+                            <div className="d-flex justify-content-center w-100">
+                                <Suspense fallback={<div>Loading...</div>}>
+                                    <LazyCheckoutForm />
+                                </Suspense>
+
+                            </div>
+                        </Row>
+                    </Container>
+                </Elements>}
+        </>
+    )
+};
 
 
 export default Payment
