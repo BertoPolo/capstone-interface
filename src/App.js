463d6544ba80ab8d7272877e642c45c0b1005b67
BertoPolo <bertopolo91@gmail.com>
2023-12-01 17:44:56 +0100

    routing paths improvement

--- src/App.js
+++ src/App.js
@@ -2,55 +2,59 @@ import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import "react-toastify/dist/ReactToastify.css"
+import React, { Suspense, lazy } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
 import Login from "./components/Login"
-import Home from "./components/Home"
-import CreateUser from "./components/CreateUser"
-import NotFound from "./components/NotFound"
-import BackOfficeMenu from "./components/BackOfficeMenu"
-import ResumingCart from "./components/ResumingCart"
-import MyAccount from "./components/MyAccount"
-import Payment from "./components/Payment"
-import ForgotPassword from "./components/ForgotPassword"
-import WhatIUsed from "./components/WhatIUsed"
+const BackOfficeMenu = lazy(() => import("./components/BackOfficeMenu"))
+const Home = lazy(() => import("./components/Home"))
+const CreateUser = lazy(() => import("./components/CreateUser"))
+const NotFound = lazy(() => import("./components/NotFound"))
+const MyAccount = lazy(() => import("./components/MyAccount"))
+const Payment = lazy(() => import("./components/Payment"))
+const ResumingCart = lazy(() => import("./components/ResumingCart"))
+const ForgotPassword = lazy(() => import("./components/ForgotPassword"))
+const WhatIUsed = lazy(() => import("./components/WhatIUsed"))
 
 const App = () => (
   <div>
     <BrowserRouter>
-      {/* Toaster */}
-      <ToastContainer
-        position="top-center"
-        autoClose={2500}
-        limit={1}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-      <Routes>
-        {/*  avoid repeating route elements */}
-        <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<Login />} />
-        <Route path="/newUser" element={<CreateUser />} />
-        <Route path="/myAccount" element={<MyAccount />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/home/:itemTitle" element={<Home />} />
-        <Route path="/home/contactUs" element={<Home />} />
-        <Route path="/home/outlet" element={<Home />} />
-        <Route path="/backOfficeMenu" element={<BackOfficeMenu />} />
-        <Route path="/backOfficeMenu/backOfficeUsers" element={<BackOfficeMenu />} />
-        <Route path="/backOfficeMenu/backOfficeItems" element={<BackOfficeMenu />} />
-        <Route path="/backOfficeMenu/backOfficeNewItem" element={<BackOfficeMenu />} />
-        <Route path="/resume" element={<ResumingCart />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/forgotPassword" element={<ForgotPassword />} />
-        <Route path="/whatiused" element={<WhatIUsed />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        {/* Toaster */}
+        <ToastContainer
+          position="top-center"
+          autoClose={2500}
+          limit={1}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/newUser" element={<CreateUser />} />
+          <Route path="/myAccount" element={<MyAccount />} />
+          <Route path="/home" element={<Home />}>
+            <Route path=":itemTitle" element={<Home />} />
+            <Route path="contactUs" element={<Home />} />
+            <Route path="outlet" element={<Home />} />
+          </Route>
+          <Route path="/backOfficeMenu" element={<BackOfficeMenu />}>
+            <Route path="backOfficeUsers" element={<BackOfficeMenu />} />
+            <Route path="backOfficeItems" element={<BackOfficeMenu />} />
+            <Route path="backOfficeNewItem" element={<BackOfficeMenu />} />
+          </Route>
+          <Route path="/resume" element={<ResumingCart />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/forgotPassword" element={<ForgotPassword />} />
+          <Route path="/whatiused" element={<WhatIUsed />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   </div>
 )
