2764646b0ae8622d50755cfc6585e0d9f39a92c0
BertoPolo <bertopolo91@gmail.com>
2024-02-12 08:11:33 +0100

    Update MyAccount.jsx

--- src/components/MyAccount.jsx
+++ src/components/MyAccount.jsx
@@ -147,7 +147,7 @@ const MyAccount = () => {
                 </Col>
               </Row>
 
-              <Row>
+              <Row className="mt-2">
                 <Col>
                   <Form.Label>Email</Form.Label>
                   <Form.Control type="email" placeholder="thomas_bernard@gmail.com" value={emailInput} onChange={(e) => setEmailInput(e.target.value)} />
@@ -159,33 +159,31 @@ const MyAccount = () => {
                 </Col>
               </Row>
 
-              <div className="d-flex justify-content-between">
+              <div className="d-flex justify-content-end mt-5">
+                <Button className="ml-3 bg-transparent text-dark border-0" onClick={() => navigate("/home")}>Cancel</Button>
                 <Button className="submitButton" type="submit"> Submit </Button>
-                <Button className="ml-3 buttonBack" onClick={() => navigate("/home")}>Back</Button>
               </div>
             </Form>
 
             {/* password form */}
             <Form className=" transparency-box p-4 mt-4 rounded" onSubmit={(e) => changePassword(e)}>
 
-              <Form.Group>
-                <Form.Control type="password" required placeholder="New password" value={passwordInput} onChange={(e) => setPasswordInput(e.target.value)} />
-              </Form.Group>
+              <Form.Label>Password Changes</Form.Label>
 
-              <Form.Group>
-                <Form.Control
-                  type="password"
-                  required
-                  placeholder="Repeat your new pass"
-                  value={passwordAgainInput}
-                  onChange={(e) => setPasswordAgainInput(e.target.value)}
-                />
-              </Form.Group>
+              <Form.Control type="password" placeholder="New password" value={passwordInput} onChange={(e) => setPasswordInput(e.target.value)} />
 
-              <div className="d-flex justify-content-between">
+              <Form.Control
+                type="password"
+                required={passwordInput} //only required if user filled the other input
+                placeholder="Repeat your new pass"
+                value={passwordAgainInput}
+                onChange={(e) => setPasswordAgainInput(e.target.value)}
+              />
+
+              {/* <div className="d-flex justify-content-between">
                 <Button type="submit" className="submitButton" disabled={(passwordInput !== passwordAgainInput) || (!passwordInput || !passwordAgainInput)} > Submit </Button>
                 <Button className="buttonBack" onClick={() => navigate("/home")}>Back</Button>
-              </div>
+              </div> */}
             </Form>
 
           </Col>
