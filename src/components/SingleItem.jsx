95a4b8fcb0cb241479ec9588616fd6bef635e9c6
BertoPolo <bertopolo91@gmail.com>
2024-04-22 19:41:29 +0200

    style /single

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -17,7 +17,6 @@ const SingleItem = () => {
   const navigate = useNavigate()
 
 
-  console.log("is on outlet?: ", selectedItem.isOutlet)
   const getCurrentBrandName = (brandID) => {
     const brandObject = brands.find(brand => brand._id === brandID)
     if (brandObject) {
@@ -37,18 +36,22 @@ const SingleItem = () => {
           <Col >
             <img src={selectedItem.image} alt={selectedItem.title} style={{ height: "35vh", objectFit: "contain" }} />
           </Col>
+
           <Col>
-            <p className="text-muted" >{getCurrentBrandName(selectedItem.brand)}</p>
+            <p className="text-muted">{getCurrentBrandName(selectedItem.brand)}</p>
 
-            <h2>{selectedItem.title}</h2>
-            <p>{selectedItem.fullDescription}</p>
+            <h2><b>{selectedItem.title}</b></h2>
 
-
-            {selectedItem.isOutlet ? <div>
-              <h3>{selectedItem.outletPrice}€</h3>
-              <h4 className="text-red"><s>{selectedItem.price}€</s></h4>
+            {selectedItem.isOutlet ? <div className="d-flex">
+              <h2 className="m-0">{selectedItem.outletPrice}€</h2>
+              <h4 className="text-red mt-auto mb-auto ml-4"><s>{selectedItem.price}€</s></h4>
             </div>
               : <h3>{selectedItem.price}€</h3>}
+
+            <hr />
+
+            <p>{selectedItem.fullDescription}</p>
+
 
             <div className="d-flex justify-content-between mt-3">
               <Button className="addToCartButton w-25 " disabled={isButtonDisabled} onClick={() => dispatch(addToCart(selectedItem))}><i className="bi bi-cart-plus"></i></Button>
