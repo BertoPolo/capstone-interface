d91ebd50344693385279e478e3b6cdbb52b0cadb
BertoPolo <bertopolo91@gmail.com>
2024-04-23 19:08:28 +0200

    start updating Footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,38 @@
 import { Row, Col } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 const Footer = () => {
   return (
     <>
       <Row className="mt-5 pt-1 footer text-center">
-        <Col >
-          <h5>
-            <u>About Us</u>
+        <Col className="">
+          <h5 className="">
+            Account
           </h5>
-          <p>
-            We are a small family company who love motorbikes since we born, and now we want to share with others our passion and support. We made this project with the purpose of approach others this lifestyle on an accessible way.
-          </p>
+          <p>My Account</p>
+          <p>Login / Register</p>
+          <p>Cart</p>
+        </Col>
+
+        <Col>
+          <h5>Social Networks</h5>
+          <Link target="_blank" to="https://www.instagram.com" rel="noopener noreferrer">
+            <i className="bi bi-instagram mr-1"></i>
+            We are on Instagram!
+          </Link>
+
+          <Link target="_blank" to="https://www.linkedin.com/in/bertopolo/" rel="noopener noreferrer">
+            <i className="bi bi-linkedin mr-1"></i>
+            We are on LinkedIn!
+          </Link>
 
         </Col>
-        <Col>
-          {/* change A tags */}
-          <h5>
-            <u>Social Networks</u>
-          </h5>
-          <p>
-            <a target="_blank" href="https://www.instagram.com" rel="noopener noreferrer">
-              <i className="bi bi-instagram mr-1"></i>
-              We are on Instagram!
-            </a>
-          </p>
-          <p>
-            <a target="_blank" href="https://www.linkedin.com/in/bertopolo/" rel="noopener noreferrer">
-              <i className="bi bi-linkedin mr-1"></i>
-              We are on LinkedIn!
-            </a>
-          </p>
 
-        </Col>
         <Col>
-          <h5>
-            <u>Contact us</u>
-          </h5>
-          <a className="d-block" href="bertopolo91@gmail.com">bertopolo91@gmail.com</a>
-          {/* <p>stufftoroute@wedontexist.com</p> */}
+          <h5>About Stuff to Route</h5>
+          <p>About us</p>
+          <p>Contact us</p>
+
         </Col>
       </Row>
     </>
