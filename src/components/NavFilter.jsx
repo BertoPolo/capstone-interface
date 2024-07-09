28e65568dc0628f6f8667279ac798ac3731c46d8
Alberto <bertopolo91@gmail.com>
2024-07-09 17:00:56 +0200

    restyle nav filter

--- src/components/NavFilter.jsx
+++ src/components/NavFilter.jsx
@@ -9,7 +9,6 @@ import { addBrands } from "../slices/brands/brandsSlice"
 
 const NavFilter = () => {
     const [searchInput, setSearchinput] = useState("")
-    const [minPrice, setMinPrice] = useState(0)
     const [maxPrice, setMaxPrice] = useState(1000)
     const [sorting, setSorting] = useState("")
     const [brandId, setBrandId] = useState("")
@@ -25,19 +24,14 @@ const NavFilter = () => {
     const navigate = useNavigate()
 
 
-    const handleInput = (e) => {
-        setMinPrice(e.minValue);
-        setMaxPrice(e.maxValue);
-    }
-
     const resetStates = () => {
         setSearchinput("")
         setMaxPrice(1000)
-        setMinPrice(0)
         setSelectSorting("")
         setSelectedBrand("")
         setSorting("")
         setBrandId("")
+        // setMinPrice(0)
 
         if (location.pathname === "/home/outlet") {
             getFilteredItems(null, true)
@@ -83,14 +77,6 @@ const NavFilter = () => {
         }
     }
 
-
-    // let filterQuery ( example :  if searchInput is true => push " &title=/^${searchInput}/i" to "filterQuery")
-    // if filter query is empty then push without &, else push with it 
-    //let filterQuery =""
-    //if (minPrice) filterQuery= filterQuery+"price>"${minPrice}  and so on..
-    // ${filterQuery}
-    // price>${minPrice}&price<${maxPrice}&sort=${sorting}&title=/^${searchInput}/&brand=${brandId}
-
     const getFilteredItems = async (e, fromResetStates = false) => {
         if (e) e.preventDefault();
         const fromOutlet = location.pathname === "/home/outlet";
@@ -98,7 +84,8 @@ const NavFilter = () => {
             let url = fromResetStates
                 //its working but shouldn't be like this? => ? `?isOutlet=true`
                 ? `?outlet=true`
-                : `?price>${encodeURIComponent(minPrice)}&price<${encodeURIComponent(maxPrice)}&sort=${encodeURIComponent(sorting)}&title=/^${encodeURIComponent(searchInput)}/i&brand=${encodeURIComponent(brandId)}${fromOutlet ? `&isOutlet="true"` : ""}`
+                : `?&sort=${encodeURIComponent(sorting)}&title=/^${encodeURIComponent(searchInput)}/i&brand=${encodeURIComponent(brandId)}${fromOutlet ? `&isOutlet="true"` : ""}`
+            // price>${encodeURIComponent(minPrice)}&price<${encodeURIComponent(maxPrice)}
 
             const response = await fetch(`${process.env.REACT_APP_SERVER}items${url}`);
             if (response.ok) {
@@ -113,98 +100,126 @@ const NavFilter = () => {
         }
     }
 
-
     useEffect(() => {
         getBrands()
     }, [])
 
     return (
         <>
-
-
-
-            <Container className="mt-4">
-                <hr />
-                <Row>
-                    {/* search bar */}
-                    <Form inline className="d-flex justify-content-center w-100 mb-3" onSubmit={(e) => getFilteredItems(e)}>
-                        <FormControl type="text" value={searchInput} placeholder="Check if we have it" className="searchBar centered-placeholder" onChange={(e) => setSearchinput(e.target.value)} />
-                        {/* <i className="bi bi-search pointer mx-3"></i> */}
-                    </Form>
-                </Row>
-            </Container>
-
             <Container className="mt-3 mb-5">
-                <Row>
-                    <Form className="d-flex justify-content-between w-100 align-items-center" onSubmit={(e) => getFilteredItems(e)}>
-                        {/*PRICE SORTING */}
-                        <Col xs={1} sm={3} className="p-0 ">
-                            <Dropdown >
-                                <Dropdown.Toggle variant="outline" >{selectSorting || "Sorting by"}</Dropdown.Toggle>
-                                <Dropdown.Menu title="Price sorting" id="basic-nav-dropdown">
+                {/*<--- XS - SM SCREENS --->*/}
+                <Form className="d-flex d-md-none justify-content-center w-100 align-items-center mb-3" onSubmit={(e) => getFilteredItems(e)}>
+                    {/* Search bar */}
+                    <Row className="w-100">
+                        <Col xs={12} className="mb-2">
+                            <div className="position-relative">
+                                <i className="bi bi-search position-absolute search-icon"></i>
+                                <FormControl
+                                    type="text"
+                                    value={searchInput}
+                                    placeholder="Check if we have it"
+                                    className="searchBar pl-5"
+                                    onChange={(e) => setSearchinput(e.target.value)}
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+                </Form>
+
+                <Form className="d-flex d-md-none justify-content-center w-100 align-items-center" onSubmit={(e) => getFilteredItems(e)}>
+                    {/* PRICE SORTING */}
+                    <Row className="w-100">
+                        <Col xs={6} sm={4} className="p-0 mb-2">
+                            <Dropdown>
+                                <Dropdown.Toggle variant="outline" className="p-0 filterDropdown custom-dropdown-toggle w-100 boxShadowblue" drop="up">
+                                    {selectSorting || "Sort by"}
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu className="filterDropdown">
                                     <Dropdown.Item onSelect={() => { setSorting("price"); setSelectSorting("Ascendant") }}>Price ascendant</Dropdown.Item>
                                     <Dropdown.Item onSelect={() => { setSorting("-price"); setSelectSorting("Descendant") }}>Price descendant</Dropdown.Item>
                                 </Dropdown.Menu>
                             </Dropdown>
-
                         </Col>
-                        {/* BY BRAND*/}
-                        <Col xs={1} sm={3} className="p-0">
-                            <Dropdown>
-                                <Dropdown.Toggle className="p-0" variant="outline">{selectedBrand || "Brand"}</Dropdown.Toggle>
 
-                                <Dropdown.Menu>
-                                    {brands.map((element) => {
-                                        return (
-                                            <Dropdown.Item key={element._id} value={element._id} onClick={() => { setSelectedBrand(element.brands); setBrandId(element._id) }} >{element.brands}</Dropdown.Item>
-                                        )
-                                    })}
+                        {/* BY BRAND */}
+                        <Col xs={6} sm={4} className="p-0 mb-2">
+                            <Dropdown>
+                                <Dropdown.Toggle className="p-0 filterDropdown w-100 boxShadowblue" variant="outline">
+                                    {selectedBrand || "Brand"}
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu className="filterDropdown">
+                                    {brands.map((element) => (
+                                        <Dropdown.Item key={element._id} value={element._id} onClick={() => { setSelectedBrand(element.brands); setBrandId(element._id) }}>
+                                            {element.brands}
+                                        </Dropdown.Item>
+                                    ))}
                                 </Dropdown.Menu>
                             </Dropdown>
-
                         </Col>
-                        {/*submit button  */}
-                        <Col xs={4} sm={3} className="d-flex justify-content-center p-0">
-                            <Button variant="outline-dark" className="pointer btn-sm" onClick={() => { resetStates() }}>Clean filters</Button>
-                            <Button type="submit" className="d-flex submitButton ml-3 btn-sm">Enter Filters</Button>
 
+                        {/* Submit buttons */}
+                        <Col xs={12} sm={4} className="d-flex justify-content-center p-0">
+                            <Button type="submit" className="d-flex submitButton mr-2 btn-sm py-2 px-3 boxShadowblue"><b>Enter</b></Button>
+                            <Button variant="outline-dark" className="pointer btn-sm py-2 px-3" onClick={() => { resetStates() }}><b>Clean</b></Button>
                         </Col>
-                        {/* BY PRICE RANGE */}
-                        {/* <Col xs={3}> */}
-                        {/* from md screens */}
-                        {/* <MultiRangeSlider
-                                min={0}
-                                max={500}
-                                step={20}
-                                minValue={minPrice}
-                                maxValue={maxPrice}
-                                onInput={(e) => {
-                                    handleInput(e);
-                                }}
-                                className="d-none d-md-block border-0 shadow-none p-0 "
-                            /> */}
+                    </Row>
+                </Form>
 
+                {/*<--- FROM MD SCREENS --->*/}
+                <Row>
+                    <Form className="d-none d-md-flex justify-content-between w-100 align-items-center" onSubmit={(e) => getFilteredItems(e)}>
+                        {/* Search bar */}
+                        <Col md={4} className="mb-2 mb-md-0">
+                            <div className="position-relative">
+                                <i className="bi bi-search position-absolute search-icon"></i>
+                                <FormControl
+                                    type="text"
+                                    value={searchInput}
+                                    placeholder="Check if we have it"
+                                    className="searchBar pl-5"
+                                    onChange={(e) => setSearchinput(e.target.value)}
+                                />
+                            </div>
+                        </Col>
 
-                        {/* <Row className="d-xs-none">
-                                <Form.Label className="align-center m-0 d-xs-none"> Min Price</Form.Label>
-                                <Form.Control type="number" value={minPrice} min="0" max="500" className="d-xs-none d-md-block w-25 mr-1" onChange={(e) => setMinPrice(e.target.value)} />
-                                <Form.Control type="number" value={maxPrice} min="0" max="500" className="d-xs-none d-md-block w-25" onChange={(e) => setMaxPrice(e.target.value)} />
-                                <Form.Label className="m-0 d-xs-none"> Max Price</Form.Label>
-                            </Row> */}
-                        {/*  */}
-                        {/* smaller screns */}
-                        {/* <Form.Control type="number" value={minPrice} min="0" max="500" className="d-md-none mr-1" onChange={(e) => setMinPrice(e.target.value)} /> */}
-                        {/* <Form.Control type="number" value={maxPrice} min="0" max="500" className="d-md-none mr-1" onChange={(e) => setMaxPrice(e.target.value)} /> */}
-                        {/*  */}
-                        {/* </Col> */}
+                        {/* PRICE SORTING */}
+                        <Col md={2} className="p-0 mb-2 mb-md-0">
+                            <Dropdown>
+                                <Dropdown.Toggle variant="outline" className=" p-0 filterDropdown custom-dropdown-toggle w-100 boxShadowblue" drop="up">
+                                    {selectSorting || "Sort by"}
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu className="filterDropdown">
+                                    <Dropdown.Item onSelect={() => { setSorting("price"); setSelectSorting("Ascendant") }}>Price ascendant</Dropdown.Item>
+                                    <Dropdown.Item onSelect={() => { setSorting("-price"); setSelectSorting("Descendant") }}>Price descendant</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Col>
+
+                        {/* BY BRAND */}
+                        <Col md={2} className="p-0 mb-2 mb-md-0">
+                            <Dropdown>
+                                <Dropdown.Toggle className="p-0 filterDropdown w-100 boxShadowblue" variant="outline">
+                                    {selectedBrand || "Brand"}
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu className="filterDropdown">
+                                    {brands.map((element) => (
+                                        <Dropdown.Item key={element._id} value={element._id} onClick={() => { setSelectedBrand(element.brands); setBrandId(element._id) }}>
+                                            {element.brands}
+                                        </Dropdown.Item>
+                                    ))}
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Col>
+
+                        {/* Submit buttons */}
+                        <Col md={4} className="d-flex justify-content-center p-0">
+                            <Button type="submit" className="d-flex submitButton mr-2 btn-sm py-2 px-3 boxShadowblue"><b>Enter</b></Button>
+                            <Button variant="outline-dark" className="pointer btn-sm py-2 px-3" onClick={() => { resetStates() }}><b>Clean</b></Button>
+                        </Col>
 
                     </Form>
-
                 </Row>
             </Container>
-
-            <hr />
-
         </>
     )
 }
